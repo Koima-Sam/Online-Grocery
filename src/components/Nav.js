24afcf4d51c2af819cc622ad0a7ff48293256890
Koima-Sam <sam17koima@gmail.com>
2022-07-26 14:43:11 +0300

    add logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,6 +8,7 @@ function Nav(){
                 <img src="https://i.pinimg.com/736x/2f/7f/dc/2f7fdcc294c90e1a98cc32e813f1723b.jpg" alt="logo"/>
                 <h2>G-<span>Sto</span>re</h2>
             </div>
+        
             <div className="nav-links">
                 <NavLink to={'/'} exact>
                     Home
