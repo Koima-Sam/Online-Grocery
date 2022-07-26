16511d0280a9b02e8e4793dd617495e4c61de197
Koima-Sam <sam17koima@gmail.com>
2022-07-26 13:16:35 +0300

    add nav-links and linked to each component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Nav(){
     return(
-        <div>
-            <h2>This is Nav Component</h2>
+        <div className="nav">
+            <div className="logo">
+                <img src="https://i.pinimg.com/736x/2f/7f/dc/2f7fdcc294c90e1a98cc32e813f1723b.jpg" alt="logo"/>
+                <h2>G-<span>Sto</span>re</h2>
+            </div>
+            <div className="nav-links">
+                <NavLink to={'/'} exact>
+                    Home
+                </NavLink>
+                <NavLink to={'/new'} exact>
+                    New Grocery
+                </NavLink>
+                <NavLink to={'/grocery-item'}>
+                    Saved Groceries
+                </NavLink>
+                
+            </div>
         </div>
     )
 }
