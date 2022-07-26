a0ab63a83df3609c33c4f1e4efd4bc2ccb021ed9
Koima-Sam <sam17koima@gmail.com>
2022-07-27 00:50:27 +0300

    return each item details

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,24 @@
 import React from "react";
+import useQuery from "../hooks/useQuery";
+import GroceryItem from "./GroceryItem";
 
 function Home(){
+    const{data:groceries} = useQuery('http://localhost:4000/groceries')
+
+    const groceryItems = groceries.map((grocery, index)=>{
+        return <GroceryItem 
+            key={index}
+            name = {grocery.name}
+            price = {grocery.price}
+            description = {grocery.description}
+            id = {grocery.id}
+            image = {grocery.image}
+        />    
+    })
+    console.log(groceries)
     return(
         <div className="groceries">
-            <h2>This is Home Component</h2>
+            {groceryItems}
         </div>
     )
 }
