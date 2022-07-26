5acc95126a3635ad0731b5a34718c609708660ce
Koima-Sam <sam17koima@gmail.com>
2022-07-26 19:52:43 +0300

    Add grocery item container

--- src/components/GroceryItem.js
+++ src/components/GroceryItem.js
@@ -1,10 +1,20 @@
 import React from "react";
 
-function GroceryItem(){
-    return(
-        <div>
-            <h2>This is GroceryItem Component</h2>
-        </div>
-    )
+function GroceryItem() {
+  return (
+    <div className="n1">
+      <img
+        src=""
+        alt="item image"
+      />
+      <h2>Red Cabbage</h2>
+      <p>Ksh per kg</p>
+      <h4>Description</h4>
+      <button>Save Item</button>
+      <button className="edit">
+        Edit Grocery <i className="far fa-edit"></i>
+      </button>
+    </div>
+  );
 }
-export default GroceryItem
+export default GroceryItem;
