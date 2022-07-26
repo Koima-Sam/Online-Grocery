2d5523b7824bc86baeb8716e52533ecbd2aaa10d
Koima-Sam <sam17koima@gmail.com>
2022-07-26 12:51:03 +0300

    Route components

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import './App.css';
 import Home from './components/Home';
 import GroceryItem from './components/GroceryItem';
 import NewGrocery from './components/NewGrocery';
+import Nav from './components/Nav';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <Home />
-      <GroceryItem />
-      <NewGrocery />
+  return <div >
+      <Nav />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/grocery-item" element={<GroceryItem />} />
+        <Route exact path="/new" element={<NewGrocery />} />
+      </Routes>
+  
     </div>
-  );
+  
 }
 
 export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Nav(){
+    return(
+        <div>
+            <h2>This is Nav Component</h2>
+        </div>
+    )
+}
+export default Nav
