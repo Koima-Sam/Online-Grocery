e5059a4cbadee93c04dc41a2113a8397b88f8829
Koima-Sam <sam17koima@gmail.com>
2022-07-27 00:50:57 +0300

    get grocery items from the api

---
+++ src/hooks/useQuery.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+
+function useQuery(url) {
+  const [data, setGroceries] = useState([]);
+  useEffect(() => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => setGroceries(data));
+  },[]);
+  return {data}
+}
+export default useQuery;
