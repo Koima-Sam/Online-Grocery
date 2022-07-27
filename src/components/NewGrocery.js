12e7f86b463d72bfb25ba46f9a9c7df288e8f974
Koima-Sam <sam17koima@gmail.com>
2022-07-27 15:39:15 +0300

    set states of the form

--- src/components/NewGrocery.js
+++ src/components/NewGrocery.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function NewGrocery(){
+    const[grocery,setGrocery]=useState(
+        {
+            name:"",
+            price:"",
+            description:"",
+            image:""
+        }
+    )
     return(
         <div className="bg">
             <div className="form">
             <h2>Please fill in the details to upload a new product</h2>
             <form className="container">
-                <input type="text" placeholder="Name of the Product"/><br/>
-                <input type="text" placeholder="Paste url of the grocery image here"/><br/>
-                <input type="text" placeholder="Price of the item per Kg"/><br/>
-                <input type="text" placeholder="Description of the grocery item"/><br/>
+                <input value ={grocery.name} type="text" placeholder="Name of the Product"/><br/>
+                <input value ={grocery.image} type="text" placeholder="Paste url of the grocery image here"/><br/>
+                <input value ={grocery.price} type="text" placeholder="Price of the item per Kg"/><br/>
+                <input value ={grocery.description} type="text" placeholder="Description of the grocery item"/><br/>
 
                 <input id="upload" type="submit" value="Upload Item"/> 
             </form>
