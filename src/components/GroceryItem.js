73dec152666d23a88bbdd98aca97c9fde857583d
Koima-Sam <sam17koima@gmail.com>
2022-07-27 19:20:50 +0300

    update database  on save

--- src/components/GroceryItem.js
+++ src/components/GroceryItem.js
@@ -1,6 +1,22 @@
 import React from "react";
 
-function GroceryItem({name,id,price,description,image}) {
+function GroceryItem({name,id,price,description,image,isSaved,onUpdate}) {
+  function handleSave(){
+    fetch(`http://localhost:4000/groceries/${id}`,{
+            method : "PATCH",
+            headers : {
+                'Content-Type':'application/json',
+                'Application' : 'application/json'
+            },
+            body:JSON.stringify({isSaved:!isSaved})
+        })
+        .then(response => response.json())
+        .then(data => {
+          console.log("Clicked......")
+          onUpdate(data,id)}
+          )
+        .catch((error)=>console.log(error))
+  }
   return (
     <div className="n1">
       <img
@@ -10,7 +26,7 @@ function GroceryItem({name,id,price,description,image}) {
       <h2>{name}</h2>
       <p>Ksh {price} per kg</p>
       <h4>{description}</h4>
-      <button>Save Item</button>
+      <button onClick={handleSave}>{isSaved? "Discard":"Save"}</button><br/>
       <button className="edit">
         Edit Grocery <i className="far fa-edit"></i>
       </button>
