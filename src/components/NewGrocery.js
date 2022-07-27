7fc73f7e002f87a5771ba49ed6a17ab0bf427c1e
Koima-Sam <sam17koima@gmail.com>
2022-07-27 15:31:22 +0300

    add form for uploading grocery

--- src/components/NewGrocery.js
+++ src/components/NewGrocery.js
@@ -2,8 +2,18 @@ import React from "react";
 
 function NewGrocery(){
     return(
-        <div>
-            <h2>This is Grocery Form Component</h2>
+        <div className="bg">
+            <div className="form">
+            <h2>Please fill in the details to upload a new product</h2>
+            <form className="container">
+                <input type="text" placeholder="Name of the Product"/><br/>
+                <input type="text" placeholder="Paste url of the grocery image here"/><br/>
+                <input type="text" placeholder="Price of the item per Kg"/><br/>
+                <input type="text" placeholder="Description of the grocery item"/><br/>
+
+                <input id="upload" type="submit" value="Upload Item"/> 
+            </form>
+        </div>
         </div>
     )
 }
