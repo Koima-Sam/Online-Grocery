e9edb73954557dcc8476414321eafcee806bcfff
Koima-Sam <sam17koima@gmail.com>
2022-07-27 17:48:19 +0300

    handle form change inputs

--- src/components/NewGrocery.js
+++ src/components/NewGrocery.js
@@ -9,15 +9,19 @@ function NewGrocery(){
             image:""
         }
     )
+    function handleChange(e){
+        e.preventDefault();
+        setGrocery({...grocery, [e.target.name] : e.target.value})
+    }
     return(
         <div className="bg">
             <div className="form">
             <h2>Please fill in the details to upload a new product</h2>
-            <form className="container">
-                <input value ={grocery.name} type="text" placeholder="Name of the Product"/><br/>
-                <input value ={grocery.image} type="text" placeholder="Paste url of the grocery image here"/><br/>
-                <input value ={grocery.price} type="text" placeholder="Price of the item per Kg"/><br/>
-                <input value ={grocery.description} type="text" placeholder="Description of the grocery item"/><br/>
+            <form className="container" onSubmit={()=>console.log('Submitted')}>
+                <input name ="name" value ={grocery.name} type="text" placeholder="Name of the Product" onChange={handleChange}/><br/>
+                <input name ="image" value ={grocery.image} type="text" placeholder="Paste url of the grocery image here" onChange={handleChange}/><br/>
+                <input name ="price" value ={grocery.price} type="text" placeholder="Price of the item per Kg" onChange={handleChange}/><br/>
+                <input name ="description" value ={grocery.description} type="text" placeholder="Description of the grocery item" onChange={handleChange}/><br/>
 
                 <input id="upload" type="submit" value="Upload Item"/> 
             </form>
