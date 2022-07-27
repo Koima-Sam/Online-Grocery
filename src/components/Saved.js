87920be7d1c41050f41cade82fb50d150df3964b
Koima-Sam <sam17koima@gmail.com>
2022-07-27 19:19:31 +0300

    get saved items

---
+++ src/components/Saved.js
@@ -0,0 +1,27 @@
+import React from "react";
+// import useQuery from "../hooks/useQuery";
+import GroceryItem from "./GroceryItem";
+
+function Saved({groceries,onUpdate}){
+    // const{data:groceries} = useQuery('http://localhost:4000/groceries')
+
+    const groceryItems = groceries.map((grocery, index)=>{
+        return <GroceryItem 
+            key={index}
+            name = {grocery.name}
+            price = {grocery.price}
+            description = {grocery.description}
+            id = {grocery.id}
+            image = {grocery.image}
+            isSaved ={grocery.isSaved}
+            onUpdate={onUpdate}
+        />    
+    })
+    // console.log(groceries)
+    return(
+        <div className="groceries">
+            {groceryItems}
+        </div>
+    )
+}
+export default Saved
